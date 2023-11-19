e7f3961225eb2da9458fdb11ad5e20b585b842f9
Jeison Gonzalez Manuyama <jeisonmanuyama@gmail.com>
2023-11-18 23:06:18 -0500

    Models Service

---
+++ Models/Service.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CiudadanosSanos.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ingrese el nombre del cliente")]
+        [Required]
+        public string Nombre { get; set; }
+
+        [DisplayName("Ingrese la descripción del servicio")]
+        [Required]
+        public string Descripcion { get; set; }
+
+        [DisplayName("Ingrese el precio del servicio")]
+        [Required]
+        public decimal Precio { get; set; }
+    }
+}
