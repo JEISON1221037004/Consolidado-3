4ae6c722b64791a14e89734379a954ac766c43f7
Jeison Gonzalez Manuyama <jeisonmanuyama@gmail.com>
2023-11-18 22:25:56 -0500

    Inicio de proyecto

---
+++ Program.cs
@@ -0,0 +1,53 @@
+using CiudadanosSanos.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+namespace CiudadanosSanos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<CSanosContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("CSanosDB"))
+            );
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+             {
+                 options.LoginPath = "/Account/Login";
+                 options.LogoutPath = "/Account/Logout";
+             });
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "usuarios",
+                    pattern: "/Account/{handler?}",
+                    defaults: new { area = "", page = "/Account/Index" }
+                );
+            });
+        }
+    }
+}
